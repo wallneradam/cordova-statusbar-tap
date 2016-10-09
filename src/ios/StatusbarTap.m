b6d022fa9c112ccd9d692b5abfbc42b4c5afea6c
Adam Wallner <wallner@bitbaro.hu>
2016-10-09 12:14:54 +0200

    - Make it work with newer Cordova versions - Comments

--- src/ios/StatusbarTap.m
+++ src/ios/StatusbarTap.m
@@ -5,21 +5,11 @@
 #import "StatusbarTap.h"
 
 @implementation StatusbarTap {
-    UIWebView *webView;
-}
-
-- (CDVPlugin*)initWithWebView:(UIWebView*)theWebView {
-    self = [super initWithWebView:theWebView];
-    if (self) {
-        initialized = NO;
-        webView = theWebView;
-    }
-    return self;
 }
 
 
 /**
- * Just a helper function to determine why scrollsToTop not working
+ * Disable scrollsToTop is all scrollViews to be able to use our own fake scrollview)
  */
 - (void)removeScrollsToTop:(UIView *)v {
     if ([v isKindOfClass:[UIScrollView class]]) {
@@ -37,23 +27,31 @@
 
 - (void)initListener:(CDVInvokedUrlCommand*)command {
     if (!initialized) {
+		// Disable scrollsToTops
         UIWindow *w = [[UIApplication sharedApplication] keyWindow];
         [self removeScrollsToTop:w.rootViewController.view];
 
+		// Create a dummy UIScrollView
         UIScrollView *sv = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, 10000, 0)];
         sv.contentSize = CGSizeMake(10000, 1);
         sv.backgroundColor = [UIColor clearColor];
         sv.contentOffset = CGPointMake(0, 1);
         sv.delegate = self;
         sv.scrollsToTop = YES;
+
+		// Add it to the webView as a subView
+        [self.webView.superview addSubview:sv];
         
-        [webView.superview addSubview:sv];
+        initialized = YES;
     }
-    initialized = YES;
 }
 
+/**
+ * Detecting statusbar taps
+ */
 - (BOOL)scrollViewShouldScrollToTop:(UIScrollView *)scrollView {
-    [webView stringByEvaluatingJavaScriptFromString:@"var evt = document.createEvent(\"Event\"); evt.initEvent(\"statusbarTap\",true,true); window.dispatchEvent(evt);"];
+    // Callback JS on statusbar tap
+    [(UIWebView *)self.webView stringByEvaluatingJavaScriptFromString:@"var evt = document.createEvent(\"Event\"); evt.initEvent(\"statusbarTap\",true,true); window.dispatchEvent(evt);"];
     return NO;
 }
 
